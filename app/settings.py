ae2853843ea720bff85f3116320cc877687eba07
fouadsan <f.benayad95@gmail.com>
2021-03-17 14:52:39 +0100

    fixed bugs

--- app/settings.py
+++ app/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '../main/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
