14a9a8e6f3d274cbf1b33b53bd463a78c54724e7
fouadsan <f.benayad95@gmail.com>
2021-03-20 18:13:04 +0100

    finalized app

--- app/settings.py
+++ app/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-DEBUG = eval(os.environ['DEBUG_VALUE'])
+DEBUG = False
 
 ALLOWED_HOSTS = ['regencyapp.herokuapp.com', '127.0.0.1']
 
